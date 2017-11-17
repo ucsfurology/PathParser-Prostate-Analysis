53205c8602d629c3d3c91479980f89f094f8ed6d
anobel odisho <anobel@users.noreply.github.com>
2017-11-16 19:55:10 -0800

    functions for 2 and 3 way comparisons function for combining 2 and 3 way into single table generating all comparisons for variables of interest into a single table

---
+++ compare_functions.r
@@ -0,0 +1,56 @@
+vars <- c("glcombined", 
+          "pathmgnpos", "pathecepos", "pathsvipos", 
+          "pathnodes_dissected", "pathnodes_positive", "pathnodes_status",
+          "tstagep", "nstagep")
+
+
+# Function to compare 2 data types against each other
+
+compare2 <- function(x, y, varname) {
+  # x is the first data source that will be used in the comparison
+  # y is the first data source that will be used in the comparison
+  # varname represents variables to compare
+  x_compare <- paste0(x,"_",varname)
+  y_compare <- paste0(y,"_",varname)
+  
+  total <- sum(!is.na(pp[,x_compare] == pp[,y_compare]))
+  correct <- sum(pp[,x_compare] == pp[,y_compare], na.rm=T)
+
+  result <- c(
+    sprintf("%.0f",total), 
+    sprintf("%.1f%%", (correct/total)*100)
+  )
+  m <- matrix(result, 1, 2)
+  colnames(m) <- c(paste(x,"vs.", y, "(n)"),paste(x,"vs.", y, "(%)"))
+  rownames(m) <- varname
+  return(m)
+}
+
+# Function to compare all 3 data types against each other
+compare3 <- function(varname) {
+  # y represents variables to compare
+  p_compare <- paste0("p_",varname)
+  s_compare <- paste0("s_",varname)
+  u_compare <- paste0("u_",varname)
+  total <- sum(!is.na(pp[,u_compare] == pp[,s_compare] & pp[,u_compare] == pp[,p_compare]))
+  correct <- sum(pp[,u_compare] == pp[,s_compare] & pp[,u_compare] == pp[,p_compare], na.rm = T)
+  
+  result <- c(
+    sprintf("%.0f",total), 
+    sprintf("%.1f%%", (correct/total)*100)
+  )
+  m <- matrix(result, 1, 2)
+  colnames(m) <- c("All 3 (n)","All 3 (%)")
+  rownames(m) <- varname
+  return(m)
+}
+
+# Functiomn to make wide table of comparisons
+allcomparisons <- function(x) {
+  cbind(
+    compare2("u", "p", x),
+    compare2("u", "s", x),
+    compare2("p", "s", x),
+    compare3(x)
+  )  
+}
--- path_prostate_clean.R
+++ path_prostate_clean.R
@@ -3,15 +3,17 @@
 library(stringr)
 
 # Import data from Coop
-pp <- read.csv("data/raw/results-201710150-deidentified.csv",
+pp <- read.csv("data/raw/SQL_QC_UODB_SDE_PARSE_03NOV2017.csv",
               header = T,
               stringsAsFactors = F,
-              na.strings = c("NULL"))
+              na.strings = "")
 
 colnames(pp) <- str_to_lower(colnames(pp))
 
 # Drop PatID (its blank, but just to be sure in future data extracts)
-pp$pat_id <- 1:nrow(pp)
+pp$de_id <- 1:nrow(pp)
+pp$ucsf_mrn <- NULL
+pp$pat_id <- NULL
 
 # Define useful vectors for field names
 v <- list()
@@ -37,7 +39,6 @@ pp[,c(v$tstagefields, v$nstagefields)] <- as.data.frame(lapply(pp[,c(v$tstagefie
 # Convert number of nodes dissected and number of nodes positive to numeric
 # will lose any that are slightly incorrect, which is OK (shows parser didnt work)
 pp[,c(v$node_dissected, v$node_pos)] <- as.data.frame(lapply(pp[,c(v$node_dissected, v$node_pos)], as.numeric))
-lapply(pp[,], is.numeric)
 
 # Convert 0/1 fields to true/false
 pp[,c(v$marginfields, v$svi_pos, v$ece_pos)] <- pp[,c(v$marginfields, v$svi_pos, v$ece_pos)] == 1
@@ -45,15 +46,57 @@ pp[,c(v$marginfields, v$svi_pos, v$ece_pos)] <- pp[,c(v$marginfields, v$svi_pos,
 # Certain UODB fields, if blank, need to be set to null
 pp$u_nstagep[pp$u_nstagep==""] <- NA
 
+###########################
 # Do we need to populate u_pathnodes_status field?
+
+# Convert tstage and nstage fields to characters
+pp$u_tstagep <- sapply(pp$u_tstagep, as.character)
+pp$p_tstagep <- sapply(pp$p_tstagep, as.character)
+pp$s_tstagep <- sapply(pp$s_tstagep, as.character)
+
+pp$u_nstagep <- sapply(pp$u_nstagep, as.character)
+pp$p_nstagep <- sapply(pp$p_nstagep, as.character)
+pp$s_nstagep <- sapply(pp$s_nstagep, as.character)
+
 
 # Drop unused levels throughout df
 pp <- droplevels(pp)
+
+# Set the BLANK fields to NA
+# Parser Gleason Tertiary
+pp$p_gtertp[pp$p_gtertp ==""] <- NA
+
+# Parser Gleason RAW
+pp$p_glsn_raw[pp$p_glsn_raw ==""] <- NA
+
+# Parser Gleason RAW
+pp$p_pathmgnpos_raw[pp$p_pathmgnpos_raw ==""] <- NA
+
+
+# SDE Gleason RAW
+pp$s_glsn_raw[pp$s_glsn_raw ==""] <- NA
 
 # Mark Fields as complete
 pp$u_glcomplete <- ifelse(!is.na(pp$u_gprimp) & !is.na(pp$u_gsecondp), T, F)
 pp$s_glcomplete <- ifelse(!is.na(pp$s_gprimp) & !is.na(pp$s_gsecondp), T, F)
 pp$p_glcomplete <- ifelse(!is.na(pp$p_gprimp) & !is.na(pp$p_gsecondp), T, F)
 pp$all_glcomplete <- ifelse(pp$u_glcomplete & pp$s_glcomplete & pp$p_glcomplete, T, F)
+
+# Create combined Gleason score Fields for easier comparisons
+pp$u_glcombined = paste0(pp$u_gprimp,"+",pp$u_gsecondp)
+pp$s_glcombined = paste0(pp$s_gprimp,"+",pp$s_gsecondp)
+pp$p_glcombined = paste0(pp$p_gprimp,"+",pp$p_gsecondp)
+
+# Remove NA fields that are generated when its missing for both
+pp$u_glcombined[pp$u_glcombined == "NA+NA"] <- NA
+pp$s_glcombined[pp$s_glcombined == "NA+NA"] <- NA
+pp$p_glcombined[pp$p_glcombined == "NA+NA"] <- NA
+
+#########################
+# need to identify where the parser had RAW text, but failed to pull a value. this isnt a NA
+# Because NA in this dataset implies there was no attempt at parsing/abstraction/SDE
+# its a MISSING value
+
+
 # Export Data
 save(pp, v, file="data/tidy/pp.rda")
